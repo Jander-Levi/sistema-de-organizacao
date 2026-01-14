0ebdf8cf73e2ea627d5fd6f7057b37dd1963602c
Jander-Levi <janderlevih@gmail.com>
2026-01-13 21:38:40 -0300

    ajuste na estrutura do projeto

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+(function () {
+  window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAtypws1SHnTm6gp7EGR0hVwSXfvE1uD_o",
+    authDomain: "system-a3710.firebaseapp.com",
+    databaseURL: "https://system-a3710-default-rtdb.firebaseio.com",
+    projectId: "system-a3710",
+    storageBucket: "system-a3710.firebasestorage.app",
+    messagingSenderId: "966989128817",
+    appId: "1:966989128817:web:95ca1e3d19b988f131e7d0",
+    measurementId: "G-T9ES35N6BP"
+  };
+})();
