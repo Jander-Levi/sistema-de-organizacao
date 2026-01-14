6b64e8d682d3a0b324f49c93f8d706e4c99d47c2
Levi Rodrigues <janderlevih@gmail.com>
2026-01-14 09:41:22 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,5 @@
 (function () {
   window.FIREBASE_CONFIG = {
-<<<<<<< HEAD
     apiKey: "AIzaSyClzdDmpnZ034yN-BC9r1ttB7hjtiwHYDA",
     authDomain: "system-a3710.firebaseapp.com",
     databaseURL: "https://system-a3710-default-rtdb.firebaseio.com",
@@ -9,15 +8,5 @@
     messagingSenderId: "966989128817",
     appId: "1:966989128817:web:daf3d662c515dc9231e7d0",
     measurementId: "G-SQKL3720MB"
-=======
-  apiKey: "AIzaSyClzdDmpnZ034yN-BC9r1ttB7hjtiwHYDA",
-  authDomain: "system-a3710.firebaseapp.com",
-  databaseURL: "https://system-a3710-default-rtdb.firebaseio.com",
-  projectId: "system-a3710",
-  storageBucket: "system-a3710.firebasestorage.app",
-  messagingSenderId: "966989128817",
-  appId: "1:966989128817:web:daf3d662c515dc9231e7d0",
-  measurementId: "G-SQKL3720MB"
->>>>>>> 6f632fa89df25ca91f2e3c4806821d806f679a82
   };
 })();
